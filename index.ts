075e3d17085ab50bfd130473ba58e13a5cdbbe8e
Richard Shade <richard@pulumi.com>
2024-10-21 15:36:54 -0500

    Adding in initial release items

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { Squid } from "./squid"
+
+const squid = new Squid(
+    "squid",
+    {
+        namespace: "default",
+        storageClassName: "civo-volume",
+        storageSize: "50Gi"
+    },
+)
